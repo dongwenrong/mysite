5816319956352cee3b07bcf579f0947242f04354
dongwenrong <154980949@qq.com>
2018-09-11 17:58:10 +0800

    20180911

--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,9 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .models import BlogArticles
+
+def blog_title(request):
+    blogs = BlogArticles.objects.all()
+    return render(request,"blog/titles.html",{"blogs":blogs})
