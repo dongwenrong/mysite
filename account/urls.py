c4b843aff8352ea1be7ee55d30574a8601ad78a2
dongwenrong <154980949@qq.com>
2018-09-13 17:56:14 +0800

    用户登录
    跟老齐学django笔记

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls import url
 from . import views
 
+from django.conf import settings
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
-    url(r'^login$',views.user_login,name="user_login"),
+    #url(r'^login/$',views.user_login,name="user_login"),
+    url(r'^login/$',auth_views.login,{"template_name":"account/login.html"},name="user_login",),
+    url(r'^new-login/$',auth_views.login,{"template_name":"registration/default_login.html"}),
+    url(r'^logout/$',auth_views.logout,name="user_logout"),
+    url(r'^pwd_reset/$',views.pwd_reset,name="pwd_reset"),
 ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^blog/', include('blog.urls',namespace='blog',app_name='blog')),
-url(r'^account/', include('account.urls',namespace='account',app_name='account')),
+    url(r'^account/', include('account.urls',namespace='account',app_name='account')),
 ]
